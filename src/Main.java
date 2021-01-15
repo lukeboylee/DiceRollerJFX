d8f5a43353298baf1ed4d06d354db4a4e52e37fa
lukeboylee <luke.boyle@ucdconnect.ie>
2021-01-15 19:36:49 +0000

    Added image for individual dice faces

--- src/Main.java
+++ src/Main.java
@@ -22,14 +22,42 @@ public class Main extends Application {
             String url = "https://wherethewindsblow.com/wp-content/uploads/2015/07/JUMBO-Six-sided-white-dice.jpg";
             Image i = new Image(url);
             ImageView iv = new ImageView(i);
-            iv.setFitHeight(100);
-            iv.setFitWidth(100);
 
             //When button is clicked generates a random number between 1 and 6, rounding down
             bt.setOnAction((ActionEvent) -> {
                 double roll = Math.floor((Math.random() *6)+1);
                 message.setText(String.valueOf((int) roll));//casts value of dice roll to int to remove decimals and sets message to this value
+
+                //Changes image displayed to a picture of the die side that was rolled
+                if(roll==1){
+                    Image i1 = new Image("https://www.pinclipart.com/picdir/middle/64-644308_similiar-dice-number-1-keywords-blank-domino-clip.png");
+                    iv.setImage(i1);
+                }
+                if(roll==2){
+                    Image i2 = new Image("https://etc.usf.edu/clipart/42100/42159/die_02_42159_lg.gif");
+                    iv.setImage(i2);
+                }
+                if(roll==3){
+                    Image i3 = new Image("https://etc.usf.edu/clipart/42100/42160/die_03_42160_mth.gif");
+                    iv.setImage(i3);
+                }
+                if(roll==4){
+                    Image i4 = new Image("http://clipart-library.com/images/5TRrMgXpc.png");
+                    iv.setImage(i4);
+                }
+                if(roll==5){
+                    Image i5 = new Image("https://etc.usf.edu/clipart/42100/42162/die_05_42162_lg.gif");
+                    iv.setImage(i5);
+                }
+                if(roll==6){
+                    Image i6 = new Image("https://etc.usf.edu/clipart/42100/42164/die_06_42164_lg.gif");
+                    iv.setImage(i6);
+                }
             });
+
+            //Sets dimensions for ImageViewer
+            iv.setFitHeight(100);
+            iv.setFitWidth(100);
 
             VBox root = new VBox(10, iv ,bt, message);  //Creates box
             root.setAlignment(Pos.CENTER);  //aligns box to centre of screen
